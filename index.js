1aeda5cb303c2b087a2707c316fdcf8fae951934
MartinGGev <66070574+MartinGGev@users.noreply.github.com>
2020-05-28 15:49:18 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('Fileloaded');
+
+// tui sha e funk 
+function martin() {
+    console.log("button clicked")
+
+    document.getElementById('heading').innerHTML = "Georgiev";
+}
+
+
+
+// click()
